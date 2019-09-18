927fc11048c73582372dfe3cc56922822a3967c5
Eric Njogu <kunadawa@gmail.com>
2019-09-18 14:05:44 +0300

    feat: adding optional args

--- detect-video-stream-test.py
+++ detect-video-stream-test.py
@@ -2,6 +2,7 @@ import unittest
 import subprocess
 import logging
 import sys
+import json
 
 class ClassName(unittest.TestCase):
     """testing detect-video-stream.py"""
@@ -18,6 +19,17 @@ class ClassName(unittest.TestCase):
         """ running file without frozen graph arg parameter should show error and return non-zero status"""
         result = subprocess.run(["python", "./detect-video-stream.py", "-"])
         self.assertNotEqual(result.returncode, 0, "there should have been an error due to missing frozen arg")
+
+    def test_optional_args(self):
+        """ test that optional args are correctly received """
+        result = subprocess.run(["python", "./detect-video-stream.py", "-", "/tmp/graph.ext", "--cutoff", "88",
+                        "--dryrun", "--classes", "1 5 8 34"], capture_output=True)
+        self.assertEqual(result.returncode, 0, "there should be no error return code")
+        args = json.loads(result.stdout)
+        self.assertEquals(args['source'], '-', "source differs")
+        self.assertEquals(args['path_to_frozen_graph'], "/tmp/graph.ext", "path to frozen graph differs")
+        self.assertEquals(args['cutoff'], "88", "cutoff score differs")
+        self.assertEquals(args['classes'], "1 5 8 34", "classes differ")
 
 if __name__ == "__main__":
     unittest.main()
--- detect-video-stream.py
+++ detect-video-stream.py
@@ -2,22 +2,32 @@
 import logging
 import os
 import argparse
+import sys
+import json
 # assemble inputs
 # run inference
 # echo output as json file - explore whether we will output numpy array or create a temp file with frame
+CUT_OFF_SCORE = 90
 
 ###
-def detect_video_stream(source='-'):
+def detect_video_stream(args):
     """ detect objects in video stream """
     # credit for retrieving current file path - http://www.karoltomala.com/blog/?p=622
-    logging.debug(f"in {os.path.abspath(__file__)}: detect_video_stream()")
-    pass
+    #logging.debug(f"in {os.path.abspath(__file__)}: detect_video_stream()")
+    # __dict__ trick from https://stackoverflow.com/a/3768975/315385
+    if args.dryrun:
+        logging.debug("dry run turned on")
+        sys.stdout.writelines(json.dumps(args.__dict__))
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
     parser = argparse.ArgumentParser(description="detect objects in video")
     # credit for adding required arg - https://stackoverflow.com/a/24181138/315385
     parser.add_argument("source", help="- for standard input or a numeral that represents the webcam device number")
-    parser.add_argument("frozen_graph", help="path to frozen model graph")
+    parser.add_argument("path_to_frozen_graph", help="path to frozen model graph")
+    parser.add_argument("--cutoff", help="cut off detection score (%)")
+    parser.add_argument("--dryrun", help="echo a params as json object, don't process anything", action="store_true")
+    parser.add_argument("--classes",
+            help="space separated list of object classes to detect as specified in label mapping")
     args = parser.parse_args()
-    detect_video_stream(args.source)
+    detect_video_stream(args)
