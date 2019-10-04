79e1711accc98e815f99a160549cd24980316b90
Eric Njogu <kunadawa@gmail.com>
2019-10-04 14:12:20 +0300

    fix: removing unused imports

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -5,17 +5,10 @@ https://github.com/tensorflow/models/blob/master/research/object_detection/objec
 
 import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
-import tarfile
 import tensorflow as tf
-import zipfile
 
 from distutils.version import StrictVersion
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
 
 if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
