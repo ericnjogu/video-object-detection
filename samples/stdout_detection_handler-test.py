a09f7e6a8338766fb9491392d5ab73c47f48ae88
Eric Njogu <kunadawa@gmail.com>
2019-10-18 16:41:20 +0300

    test: verifying expected message data types

---
+++ samples/stdout_detection_handler-test.py
@@ -0,0 +1,20 @@
+import pytest
+import grpc
+import datetime
+import numpy
+
+from proto.generated import detection_handler_pb2
+
+def test_create_handle_detection_request():
+    #TODO this same data could be made available using a fixture to test the server implementation
+    msg = detection_handler_pb2.handle_detection_request(
+                start_timestamp = datetime.datetime.now().timestamp(),
+                detection_output = {'detection_scores': [5.0]},
+                instance_name = "testing",
+                frame = [[5.0, 2.1, 3.3], [8.4, 7.9, 5.2]],
+                frame_count = 1619,
+                source = "steam")
+
+
+def test_create_handle_detection_response():
+    response = detection_handler_pb2.handle_detection_response(status=True)
