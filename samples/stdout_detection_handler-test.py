f14558b930606053acc50dea79f2c09a071395b6
Eric Njogu <kunadawa@gmail.com>
2019-10-18 18:04:00 +0300

    fix: replacing tensor.proto protobuf message with custom ones
    Each numpy array will have to be converted to these types so that the 
    message can be created successfully

--- samples/stdout_detection_handler-test.py
+++ samples/stdout_detection_handler-test.py
@@ -9,9 +9,10 @@ def test_create_handle_detection_request():
     #TODO this same data could be made available using a fixture to test the server implementation
     msg = detection_handler_pb2.handle_detection_request(
                 start_timestamp = datetime.datetime.now().timestamp(),
-                detection_output = {'detection_scores': [5.0]},
+                detection_output = {'detection_scores': detection_handler_pb2.float_array(numbers=[5.0])},
                 instance_name = "testing",
-                frame = [[5.0, 2.1, 3.3], [8.4, 7.9, 5.2]],
+                frame = [detection_handler_pb2.float_array(numbers=[5.0, 2.1, 3.3]),
+                         detection_handler_pb2.float_array(numbers=[8.4, 7.9, 5.2])],
                 frame_count = 1619,
                 source = "steam")
 
