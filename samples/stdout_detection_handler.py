5c0dcd3f8366e1b3861e560ff710f4e2a2b74c0b
Eric Njogu <kunadawa@gmail.com>
2019-10-18 13:48:05 +0300

    feat: implementing a detection handler that writes to standard output

---
+++ samples/stdout_detection_handler.py
@@ -0,0 +1,30 @@
+import grpc
+import sys
+import time
+from concurrent import futures
+
+from proto.generated import detection_handler_pb2_grpc, detection_handler_pb2
+
+class StdoutDetectionHandler(detection_handler_pb2_grpc.DetectionHandlerServicer):
+    def handle_detection(self, request, context):
+      """
+      handle a detection output
+      """
+      sys.stdout.writelines(str(request))
+
+# credit - https://www.semantics3.com/blog/a-simplified-guide-to-grpc-in-python-6c4e25f0c506/
+# create server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+# add implementing class to server
+detection_handler_pb2_grpc.add_DetectionHandlerServicer_to_server(StdoutDetectionHandler(), server);
+# listen
+port  = 50051
+print (f'starting server on port {port}')
+server.add_insecure_port(f'[::]:{port}')
+server.start()
+# sleep loop
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
