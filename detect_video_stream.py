a32b15d1d37cc71467605b98e6d3de503639bac7
Eric Njogu <kunadawa@gmail.com>
2019-10-23 09:57:41 +0300

    feat: replacing stdout with grpc calls to a detection handler

--- detect_video_stream.py
+++ detect_video_stream.py
@@ -5,14 +5,21 @@ import argparse
 import sys
 import json
 import cv2
-import object_detection as obj_detect
 import numpy as np
 import tempfile
 from datetime import datetime as dt
 import platform
+import grpc
+
+from proto.generated import detection_handler_pb2_grpc, detection_handler_pb2
+import object_detection as obj_detect
 
 CUT_OFF_SCORE = 90.0
 SAMPLE_RATE = 5
+HANDLER_PORT = 50051
+
+# TODO - move the determine* methods to a util module
+# introduce a determine_port method
 
 def determine_instance_name(instance_name):
     """
@@ -114,6 +121,9 @@ def determine_source(args, video_reader):
 
 def detect_video_stream(args):
     """ detect objects in video stream """
+    # setup grpc comms
+    channel = grpc.insecure_channel(f'localhost:{HANDLER_PORT}')
+    stub = detection_handler_pb2_grpc.DetectionHandlerStub(channel)
     # __dict__ trick from https://stackoverflow.com/a/3768975/315385
     if args.dryrun:
         sys.stdout.writelines(json.dumps(args.__dict__))
@@ -122,29 +132,39 @@ def detect_video_stream(args):
     detection_graph = obj_detect.load_frozen_model_into_memory(args.path_to_frozen_graph)
     # determine sample rate
     sample_rate = determine_samplerate(args.samplerate)
-    # adapted from https://github.com/juandes/pikachu-detection/blob/master/detection_video.py
     cap = determine_source(args, cv2.VideoCapture)
-    # TODO accept a switch that will change from video output to text output
-
     frame_count = 0
     start_time = dt.now().timestamp()
     cut_off_score = determine_cut_off_score(args)
     logging.debug(f"using a cut off score of {cut_off_score}")
     # loop over frames in video
+    # adapted from https://github.com/juandes/pikachu-detection/blob/master/detection_video.py
     while cap.isOpened():
         ret, frame = cap.read()
         # only consider frames that are a multiple of the sample rate
         if frame_count % sample_rate == 0:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img_to_array = np.expand_dims(frame, axis=0)
+            frame_bgr2rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img_to_array = np.expand_dims(frame_bgr2rgb, axis=0)
             # run inference
             output_dict = obj_detect.run_inference_for_single_image(img_to_array, detection_graph)
-            # TODO filter for classes, cut off score
+            # filter for cut off score
             output_dict = filter_detection_output(output_dict, cut_off_score)
+            # TODO
+            # convert the output dict entries into float_array - could handle the one dim separately in comprehension
+            # investigate how we can call float_array on each element in a numpy array - at a certain level without deep (for arrays only)
             if len(output_dict['detection_boxes']) > 0:
-                sys.stdout.writelines(str({'start_time': start_time,'output_dict':output_dict,
-                    'name': determine_instance_name(args.instance_name), 'frame': frame,
-                    'frame_count':frame_count, 'source':determine_source_name(args.source)}))
+                output_dict['detection_boxes'] = [detection_handler_pb2.float_array(numbers=array) for array in output_dict['detection_boxes']]
+                message = detection_handler_pb2.handle_detection_request(
+                            start_timestamp = start_time,
+                            detection_classes = output_dict['detection_classes'],
+                            detection_scores = output_dict['detection_scores'],
+                            detection_boxes = output_dict['detection_boxes'],
+                            instance_name = determine_instance_name(args.instance_name),
+                            frame = [detection_handler_pb2.float_array(numbers=array) for array in frame[0]],
+                            frame_count = frame_count,
+                            source = determine_source_name(args.source))
+                response = stub.handle_detection(message)
+                logging.debug(f"detection handler response is {response.status}")
             else:
                 logging.debug("no score was above cut-off, skipping")
             logging.debug(f"just finished frame: {frame_count}")
