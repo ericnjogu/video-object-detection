814a39a658c07ab8733f642c44096ef7a17516ca
Eric Njogu <kunadawa@gmail.com>
2019-10-23 10:05:18 +0300

    test: checking the serialized frame has the expected structure

--- samples/stdout_detection_handler-test.py
+++ samples/stdout_detection_handler-test.py
@@ -13,7 +13,7 @@ def test_create_handle_detection_request():
     frame_array1 = [5, 2, 3]
     frame_array1_nd = numpy.array(frame_array1)
     # not sure why the frame from opencv is nested this way
-    frame = [[frame_array1_nd, numpy.array([8.4, 7.9, 5.2])]]
+    frame = [[frame_array1_nd, numpy.array([8.4, 7.9, 5.2]), numpy.array([59,  64,  64])]]
     detection_output = {'detection_scores': [0.54], 'detection_classes':[8],
                         'detection_boxes':[numpy.array([0.36190858, 0.11737314, 0.94603133, 0.3205647]),
                                             numpy.array([0.345639  , 0.69829893, 0.38075703, 0.7310691 ])]}
@@ -28,6 +28,7 @@ def test_create_handle_detection_request():
                 frame = [detection_handler_pb2.float_array(numbers=array) for array in frame[0]],
                 frame_count = 1619,
                 source = "steam")
+    assert len(msg.frame) == 3
     assert msg.frame[0].numbers == frame_array1
     assert len(msg.detection_boxes) == 2
 
