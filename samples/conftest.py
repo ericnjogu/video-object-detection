e77c17f12f3dacac68245c227dc0f485e7002122
Eric Njogu <kunadawa@gmail.com>
2019-10-28 15:49:02 +0300

    test: moving fixture to separate file for possible reuse

---
+++ samples/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy
+from proto.generated import detection_handler_pb2
+import datetime
+
+@pytest.fixture
+def create_handle_detection_request():
+    """
+    returns a detection_handler_pb2.handle_detection_request object if the input data is successfully serialized into protobuf types
+    """
+    frame_array1 = [5, 2, 3]
+    # not sure why the frame from opencv is nested this way
+    frame = numpy.array([[frame_array1, [8.4, 7.9, 5.2], [59,  64,  64]]])
+    detection_output = {'detection_scores': [0.54], 'detection_classes':[8],
+                        'detection_boxes':numpy.array([[0.36190858, 0.11737314, 0.94603133, 0.3205647],
+                                            [0.345639  , 0.69829893, 0.38075703, 0.7310691 ]])}
+    detection_boxes = detection_handler_pb2.float_array(numbers=detection_output['detection_boxes'].ravel(),
+                                                        shape=detection_output['detection_boxes'].shape)
+    string_map={'color':'blue', 'music':'classical'}
+    float_map={'weight':56.9, 'height':85.4}
+    return detection_handler_pb2.handle_detection_request(
+                start_timestamp = datetime.datetime.now().timestamp(),
+                detection_scores = detection_output['detection_scores'],
+                detection_classes = detection_output['detection_classes'],
+                detection_boxes = detection_boxes,
+                instance_name = "testing",
+                frame = detection_handler_pb2.float_array(numbers=frame.ravel(), shape=frame.shape),
+                frame_count = 1619,
+                source = "steam",
+                string_map=string_map,
+                float_map=float_map), string_map, float_map
--- samples/stdout_detection_handler-test.py
+++ samples/stdout_detection_handler-test.py
@@ -8,30 +8,12 @@ from proto.generated import detection_handler_pb2
 
 # to enable log statements on CLI run with python -m pytest samples/stdout_detection_handler-test.py --log-cli-level=DEBUG
 
-def test_create_handle_detection_request():
-    # TODO this same data could be made available using a fixture to test the server implementation
-    frame_array1 = [5, 2, 3]
-    # not sure why the frame from opencv is nested this way
-    frame = numpy.array([[frame_array1, [8.4, 7.9, 5.2], [59,  64,  64]]])
-    detection_output = {'detection_scores': [0.54], 'detection_classes':[8],
-                        'detection_boxes':numpy.array([[0.36190858, 0.11737314, 0.94603133, 0.3205647],
-                                            [0.345639  , 0.69829893, 0.38075703, 0.7310691 ]])}
-    detection_boxes = detection_handler_pb2.float_array(numbers=detection_output['detection_boxes'].ravel(),
-                                                        shape=detection_output['detection_boxes'].shape)
-    string_map={'color':'blue', 'music':'classical'}
-    float_map={'weight':56.9, 'height':85.4}
-    # logging.debug(f"detection_boxes: {detection_boxes}")
-    msg = detection_handler_pb2.handle_detection_request(
-                start_timestamp = datetime.datetime.now().timestamp(),
-                detection_scores = detection_output['detection_scores'],
-                detection_classes = detection_output['detection_classes'],
-                detection_boxes = detection_boxes,
-                instance_name = "testing",
-                frame = detection_handler_pb2.float_array(numbers=frame.ravel(), shape=frame.shape),
-                frame_count = 1619,
-                source = "steam",
-                string_map=string_map,
-                float_map=float_map)
+def test_create_handle_detection_request(create_handle_detection_request):
+    """
+    Tests that the input data is serialized okay into the protobuf types without errors
+    Receives a fixture function defined in conftest.py which is loaded by pytest
+    """
+    msg, string_map, float_map = create_handle_detection_request
     assert len(msg.frame.numbers) == 9
     assert msg.frame.shape == [1, 3, 3]
 
@@ -41,7 +23,8 @@ def test_create_handle_detection_request():
     logging.debug(ndarray)
 
     assert msg.string_map == string_map
-    assert msg.float_map == string_map
+    for k, v in msg.float_map.items():
+        assert v == pytest.approx(float_map[k])
 
 
 
