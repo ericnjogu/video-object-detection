c0c2d713feb8321f9e0d93e97f0ed0aaeaf09888
Eric Njogu <kunadawa@gmail.com>
2019-10-30 16:33:40 +0300

    test: adjusting fixture detection boxes and adding a category index
    adjusting detection boxes to match number of detections, adding category 
    index to match the detected classes

--- samples/conftest.py
+++ samples/conftest.py
@@ -13,12 +13,12 @@ def create_handle_detection_request():
     # not sure why the frame from opencv is nested this way
     frame = numpy.array([[frame_array1, [8.4, 7.9, 5.2], [59,  64,  64]]])
     detection_output = {'detection_scores': [0.54], 'detection_classes':[8],
-                        'detection_boxes':numpy.array([[0.36190858, 0.11737314, 0.94603133, 0.3205647],
-                                            [0.345639  , 0.69829893, 0.38075703, 0.7310691 ]])}
+                        'detection_boxes':numpy.array([[0.36190858, 0.11737314, 0.94603133, 0.3205647]])}
     detection_boxes = detection_handler_pb2.float_array(numbers=detection_output['detection_boxes'].ravel(),
                                                         shape=detection_output['detection_boxes'].shape)
     string_map={'color':'blue', 'music':'classical'}
     float_map={'weight':56.9, 'height':85.4}
+    category_index = {8: {'name':"elephant"}}
     return detection_handler_pb2.handle_detection_request(
                 start_timestamp = datetime.datetime.now().timestamp(),
                 detection_scores = detection_output['detection_scores'],
@@ -29,4 +29,4 @@ def create_handle_detection_request():
                 frame_count = 1619,
                 source = "steam",
                 string_map=string_map,
-                float_map=float_map), string_map, float_map
+                float_map=float_map), string_map, float_map, category_index
