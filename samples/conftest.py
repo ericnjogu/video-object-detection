522e9f4784bef19f39ebb803f8821a63091ea7a5
Eric Njogu <kunadawa@gmail.com>
2019-11-11 12:55:47 +0300

    style: making PEP 8 style changes

--- samples/conftest.py
+++ samples/conftest.py
@@ -11,23 +11,23 @@ def create_handle_detection_request():
     """
     frame_array1 = [5, 2, 3]
     # not sure why the frame from opencv is nested this way
-    frame = numpy.array([[frame_array1, [8.4, 7.9, 5.2], [59,  64,  64]]])
-    detection_output = {'detection_scores': [0.54], 'detection_classes':[8],
-                        'detection_boxes':numpy.array([[0.36190858, 0.11737314, 0.94603133, 0.3205647]])}
+    frame = numpy.array([[frame_array1, [8.4, 7.9, 5.2], [59, 64, 64]]])
+    detection_output = {'detection_scores': [0.54], 'detection_classes': [8],
+                        'detection_boxes': numpy.array([[0.36190858, 0.11737314, 0.94603133, 0.3205647]])}
     detection_boxes = detection_handler_pb2.float_array(numbers=detection_output['detection_boxes'].ravel(),
                                                         shape=detection_output['detection_boxes'].shape)
-    string_map={'color':'blue', 'music':'classical'}
-    float_map={'weight':56.9, 'height':85.4}
-    category_index = {8: {'name':"elephant"}}
+    string_map = {'color': 'blue', 'music': 'classical'}
+    float_map = {'weight': 56.9, 'height': 85.4}
+    category_index = {8: {'name': "elephant"}}
     return detection_handler_pb2.handle_detection_request(
-                start_timestamp = datetime.datetime.now().timestamp(),
-                detection_scores = detection_output['detection_scores'],
-                detection_classes = detection_output['detection_classes'],
-                detection_boxes = detection_boxes,
-                instance_name = "testing",
-                frame = detection_handler_pb2.float_array(numbers=frame.ravel(), shape=frame.shape),
-                frame_count = 1619,
-                source = "steam",
-                string_map=string_map,
-                float_map=float_map,
-                category_index={k:v['name'] for k, v in category_index.items()}), string_map, float_map, category_index
+        start_timestamp=datetime.datetime.now().timestamp(),
+        detection_scores=detection_output['detection_scores'],
+        detection_classes=detection_output['detection_classes'],
+        detection_boxes=detection_boxes,
+        instance_name="testing",
+        frame=detection_handler_pb2.float_array(numbers=frame.ravel(), shape=frame.shape),
+        frame_count=1619,
+        source="steam",
+        string_map=string_map,
+        float_map=float_map,
+        category_index={k: v['name'] for k, v in category_index.items()}), string_map, float_map, category_index
